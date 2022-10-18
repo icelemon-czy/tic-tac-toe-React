29916fea5ec19c3d43ad2512d65ffa1d2b3c3c28
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-10-17 21:10:42 -0400

    update

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import './App.css';
 import Game from "./components/Game";
 import style from "./style.css";
 function App() {
-  return (
-      <div>
-          <body>
-          <h1>
-              Tic Tac Toe
-          </h1>
-              <script type="text/javascript" src="//code.tidio.co/tw5rt6w4tf1lyeaugtmkd45wqw3zzpdm.js" async></script>
-          </body>
-        <Game />
-      </div>
-  );
+    return (
+        <body>
+        <div>
+            <h1>
+                Tic Tac Toe
+            </h1>
+            <script type="text/javascript" src="//code.tidio.co/tw5rt6w4tf1lyeaugtmkd45wqw3zzpdm.js" async></script>
+
+            <Game/>
+        </div>
+        </body>
+    );
 }
 
 export default App;
