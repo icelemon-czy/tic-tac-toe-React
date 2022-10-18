5056de620337883e7fd3dd8852a59b85cb56047d
icelemon-czy <71572625+icelemon-czy@users.noreply.github.com>
2022-10-17 20:41:11 -0400

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import style from "./style.css";
 function App() {
   return (
       <div>
+          <div>
+              <script src="//code.tidio.co/tw5rt6w4tf1lyeaugtmkd45wqw3zzpdm.js" async></script>
+          </div>
         <Game />
       </div>
   );
